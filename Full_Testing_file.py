7d3fcdbdf897f1ef061bb010f269edd881081d09
yiyonglok <122137063+yiyonglok@users.noreply.github.com>
2023-02-09 20:57:19 -0500

    Add files via upload

---
+++ Full_Testing_file.py
@@ -0,0 +1,16 @@
+
+#Image Processing
+#read in the image as numpy array/matrix (must be grayscaled)
+#take and save 32x32 offset samples of an image (1-5 pixels offset?)
+#break 32x32 samples in to smaller 8x8 samples for k-means prep
+#pass image dimensions through
+
+#K-Tester file
+#feed the array of samples into k-mean testing aglo to get equivalent centroid locations
+
+#MLP-tester file
+#Put the 8x8 samples back together and flatten to reform 32x32 centroid representations of the samples
+#Feed through Testing MLP
+#visualized representation of where letters are thought to be on the image
+
+
