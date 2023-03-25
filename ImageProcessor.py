d02878a7a7d84e286abee4505c908742b4d8bf69
Deanna <47610255+deannastender@users.noreply.github.com>
2023-03-24 21:25:25 -0400

    Added code to resize test images

--- ImageProcessor.py
+++ ImageProcessor.py
@@ -1,7 +1,11 @@
 import numpy
 import skimage.io
-from PIL import Image
+from PIL import Image, ImageOps
 import os
+
+MAX_RESOLUTION = (24, 24)
+REDUCTION_FACTOR = 8
+
 
 def get_image_paths(path):
     files = []
@@ -10,6 +14,7 @@ def get_image_paths(path):
             if '.png' in file or '.jpg' in file:
                 files.append(os.path.join(r, file))
     return files
+
 
 def image_name_parser(filename):
     file_path = filename.split("\\")
@@ -22,7 +27,7 @@ def read_letter_images(filepath):
     data_array = []
     progress = 0
     for file in load_letters:
-        print(f"Progress: {progress*100/len(load_letters)}%")
+        print(f"Progress: {progress * 100 / len(load_letters)}%")
         if filepath != "non_letters":
             classifier = image_name_parser(file)
         else:
@@ -36,14 +41,13 @@ def read_letter_images(filepath):
         progress += 1
 
     data_array = numpy.array(data_array)
-    # print(data_array[-1])
 
-    # numpy.save("Unshuffled_datafile", data_array)
     return data_array
+
 
 def image_resize(image_path):
     image = Image.open(image_path)
-    new_image = image.resize((32,32))
+    new_image = image.resize((32, 32))
     new_image.save(image_path)
 
 
@@ -58,7 +62,7 @@ def image_whitener(file):
     new_file = new_file_path[-1].split(".")
     s = "\\"
     new_file_path = s.join(new_file_path[:-1])
-    new_file_path = new_file_path + "\\" +str(new_file[0]) + "_whitened.jpg"
+    new_file_path = new_file_path + "\\" + str(new_file[0]) + "_whitened.jpg"
     print(new_file_path)
     image.save(new_file_path)
 
@@ -69,30 +73,27 @@ def sampler(image, classifier):
     for column in range(0, len(image[0]), 8):
         for row in range(0, len(image[0]), 8):
             image_sample.append(image[column:column + 8, row:row + 8])
-            temp_array.append(numpy.concatenate((numpy.array([num for sublist in image_sample[-1] for num in sublist]), [classifier])))
+            temp_array.append(numpy.concatenate(
+                (numpy.array([num for sublist in image_sample[-1] for num in sublist]), [classifier])))
     return temp_array
 
 
 def delete_bad_image(file):
     print(file)
     if "whitened.jpg" in file:
-        print("yes")
         os.remove(file)
 
 
 def read_nonletter_images(filepath):
     load_nonletters = get_image_paths(filepath)
-    print(len(load_nonletters))
-    #load_nonletters = load_nonletters[:2]
 
     data_array = []
     count = 0
     for file in load_nonletters:
-        print("Progress:", str(count*100/len(load_nonletters)), "%")
+        print("Progress:", str(count * 100 / len(load_nonletters)), "%")
 
         classifier = 0
 
-        #print(file)
         nonletter = skimage.io.imread(file, as_gray=True)
 
         if len(data_array) == 0:
@@ -102,15 +103,6 @@ def read_nonletter_images(filepath):
         count += 1
 
     return data_array
-
-    #print(len(data_array))
-    #print(len(data_array[0]))
-    #print(len(data_array))
-
-    # iimage = Image.fromarray(data_array[0])
-    # if iimage.mode != 'RGB':
-    #     iimage = iimage.convert('RGB')
-    # iimage.save("test-pic-2.jpg")
 
 
 def add_border(image):
@@ -140,22 +132,51 @@ def average_pooling(image):
     return numpy.array(new_image)
 
 
+def shrink_lowercase_samples(file_path):
+    files = get_image_paths(file_path)
+
+    for file in files:
+        with Image.open(file) as img:
+            print(img)
+            # get image dimensions, grayscale, load pixel data
+            img = resize_image(img)
+            # letter = numpy.array(img)
+            img.save(f"{file}")
+
+
+def resize_image(img):
+    width, height = 32, 32
+    max_width, max_height = MAX_RESOLUTION
+    if (width > max_width) or (height > max_height):
+        img = img.resize((width - REDUCTION_FACTOR, height - REDUCTION_FACTOR))
+    return img
+
+
+def add_lowercase_border(file_path):
+    files = get_image_paths(file_path)
+
+    for file in files:
+        small_image = skimage.io.imread(file, as_gray=True)
+        small_image = numpy.array(small_image)
+        border_size = int(REDUCTION_FACTOR / 2)
+        filler = numpy.full((int(border_size), 32 - REDUCTION_FACTOR), 255)
+        small_image = numpy.concatenate((filler, small_image), axis=0)
+        small_image = numpy.concatenate((small_image, filler), axis=0)
+        filler = numpy.full((32, (int(border_size))), 255)
+        small_image = numpy.concatenate((filler, small_image), axis=1)
+        small_image = numpy.concatenate((small_image, filler), axis=1)
+        save_letter(small_image, file)
+
+
+def save_letter(image_matrix, name):
+    image_matrix = numpy.array(image_matrix)
+    image = Image.fromarray(image_matrix)
+    if image.mode != 'RGB':
+        image = image.convert('RGB')
+    file_path = f"{name}"
+    image.save(file_path)
+
+
 if __name__ == "__main__":
-    #letters = read_letter_images()
-    nonletters = read_nonletter_images()
-
-    #letters_length = len(letters)
-    nonletters_length = len(nonletters)
-
-    #print(len(letters))
-    print(len(nonletters))
-
-    #unshuffled_data = numpy.concatenate((letters, nonletters), axis=0)
-    numpy.save("nonletter_data_array.npy", nonletters)
-
-    #class_one = numpy.full((letters_length, 1), 1)
-    #class_two = numpy.full((nonletters_length, 1), 0)
-    #classes = numpy.concatenate((class_one, class_two), axis=0)
-
-    #unshuffled_data = numpy.concatenate((unshuffled_data, classes), axis=1)
-    #numpy.save("unshuffled_labeled_data", unshuffled_data)
+    shrink_lowercase_samples("Char74k_32x32_cleaned_deanna")
+    add_lowercase_border("Char74k_32x32_cleaned_deanna")
